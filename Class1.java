be0b60151ad6e753be0465e1f9b71a3e1add8451
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-22 23:15:44 +0800

    Update Class1.java

--- Class1.java
+++ Class1.java
@@ -4,4 +4,28 @@ import java.util.ArrayList;
 
 public class Learning {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // instantiate variables
+
+        // Syntax - programming grammar
+        // Syntax Error -> problem grammar 
+        // Logic Error -> problem business logic 
+
+        // Primitive Datatype: String, int, boolean, float, double, Array -> Referencing
+        // Datatype : String, int, boolean, Array[], etc... (Class)
+        // Syntax: {Datatype} {variable_name} = {value};
+
+        String item = "hi";
+
+        //System.out.println(item);
+
+        //List<> ArrayList<>
+        // Syntax: ArrayList<{Datatype}> {variable_name} = new ArrayList<>();   int vs Integer
+        // 
+        ArrayList<Integer> arr = new ArrayList<>(); // Generic Class
+        arr.add(1); // index start 0
+        arr.add(2);
+        arr.add(3);
+
+        // .add() .get()
+
+        // for ({start_value}; {conditions}; {increment})
