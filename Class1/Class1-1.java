7559c754c7efdc00a4f8c52107ac6322ad15d1b1
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-24 01:28:35 +0800

    Class1

--- Class1/Class1-1.java
+++ Class1/Class1-1.java
@@ -1,22 +1,22 @@
-public static void main(String []args){
+// public static void main(String []args){
 
-        // Given an array: [1, 2, 3, 4, 5], please sort by descending order and store into a new array.
+//         // Given an array: [1, 2, 3, 4, 5], please sort by descending order and store into a new array.
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(3);
-        arr.add(4);
-        arr.add(5); // <- i start here
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(1);
+//         arr.add(2);
+//         arr.add(3);
+//         arr.add(4);
+//         arr.add(5); // <- i start here
 
-        ArrayList<Integer> newArr = new ArrayList<>(); // local variable
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            // local variable
-            newArr.add(arr.get(i));
-        }
+//         ArrayList<Integer> newArr = new ArrayList<>(); // local variable
+//         for (int i = arr.size() - 1; i >= 0; i--) {
+//             // local variable
+//             newArr.add(arr.get(i));
+//         }
 
-        for (int i = 0; i < newArr.size(); i++) {
-            System.out.println(newArr.get(i));
-        }
+//         for (int i = 0; i < newArr.size(); i++) {
+//             System.out.println(newArr.get(i));
+//         }
 
-     }
+//      }
--- Class1/Class1.java
+++ Class1/Class1.java
@@ -1,103 +1,103 @@
-package com;
+// package com;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-public class Learning {
+// public class Learning {
 
-    public static void main(String[] args) {        // instantiate variables
+//     public static void main(String[] args) {        // instantiate variables
 
-        // Syntax - programming grammar
-        // Syntax Error -> problem grammar 
-        // Logic Error -> problem business logic 
+//         // Syntax - programming grammar
+//         // Syntax Error -> problem grammar 
+//         // Logic Error -> problem business logic 
 
-        // Primitive Datatype: String, int, boolean, float, double, Array -> Referencing
-        // Datatype : String, int, boolean, Array[], etc... (Class)
-        // Syntax: {Datatype} {variable_name} = {value};
+//         // Primitive Datatype: String, int, boolean, float, double, Array -> Referencing
+//         // Datatype : String, int, boolean, Array[], etc... (Class)
+//         // Syntax: {Datatype} {variable_name} = {value};
 
-        String item = "hi";
+//         String item = "hi";
 
-        //System.out.println(item);
+//         //System.out.println(item);
 
-        //List<> ArrayList<>
-        // Syntax: ArrayList<{Datatype}> {variable_name} = new ArrayList<>();   int vs Integer
-        // 
-        ArrayList<Integer> arr = new ArrayList<>(); // Generic Class
-        arr.add(1); // index start 0
-        arr.add(2);
-        arr.add(3);
+//         //List<> ArrayList<>
+//         // Syntax: ArrayList<{Datatype}> {variable_name} = new ArrayList<>();   int vs Integer
+//         // 
+//         ArrayList<Integer> arr = new ArrayList<>(); // Generic Class
+//         arr.add(1); // index start 0
+//         arr.add(2);
+//         arr.add(3);
 
-        // .add() .get()
+//         // .add() .get()
 
-        // for ({start_value}; {conditions}; {increment})
+//         // for ({start_value}; {conditions}; {increment})
 
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }
+//         for (int i = 0; i < arr.size(); i++) {
+//             System.out.println(arr.get(i));
+//         }
 
-        if (false) { // nested if
+//         if (false) { // nested if
 
-            if (false) {
+//             if (false) {
 
-            } else {
+//             } else {
 
-            }
-       } else if (false) {
-        } else if (false) {
-        } else if (false) {
-        } else if (false) {
-        } else if (false) {
-        } else if (false) {
-        } else if (false) {
+//             }
+//        } else if (false) {
+//         } else if (false) {
+//         } else if (false) {
+//         } else if (false) {
+//         } else if (false) {
+//         } else if (false) {
+//         } else if (false) {
 
-        } else {
+//         } else {
 
-        }
-
-
-        if (false) { // nested if -> elakkan: Guard clauses
-
-            if (false) {
-
-                if (false) {
-
-                }
-
-            }
-
-        }
-
-        // Switch
-        int x = 1;
-        switch (x) {
-            case 1: System.out.println(); break;
-            case 2: break;
-            default: break;
-        }
+//         }
 
 
+//         if (false) { // nested if -> elakkan: Guard clauses
 
-    }
+//             if (false) {
 
-}
+//                 if (false) {
 
-import java.util.ArrayList;
-public class HelloWorld{
+//                 }
 
-     public static void main(String []args){
-        System.out.println("Hello World");
-        System.out.println("Test");
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(3);
-        arr.add(4);
-        arr.add(5);
+//             }
+
+//         }
+
+//         // Switch
+//         int x = 1;
+//         switch (x) {
+//             case 1: System.out.println(); break;
+//             case 2: break;
+//             default: break;
+//         }
 
 
 
-        for (int i = 4; i >= 0; i--) {
-            arr.add(i);
-            System.out.println(i);
-        }
-    }
-}
+//     }
+
+// }
+
+// import java.util.ArrayList;
+// public class HelloWorld{
+
+//      public static void main(String []args){
+//         System.out.println("Hello World");
+//         System.out.println("Test");
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(1);
+//         arr.add(2);
+//         arr.add(3);
+//         arr.add(4);
+//         arr.add(5);
+
+
+
+//         for (int i = 4; i >= 0; i--) {
+//             arr.add(i);
+//             System.out.println(i);
+//         }
+//     }
+// }
