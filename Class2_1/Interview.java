c7b5a01993c0609aeef253f6c81c15bd2481fc3c
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-24 01:38:02 +0800

    Create Interview.java

---
+++ Class2_1/Interview.java
@@ -0,0 +1,24 @@
+package Class2_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Interview {
+    
+    // Process filtering
+    // Input - Process - Output
+    // All Applicants (List) - {process()} - Passed Applicants (List) 
+    
+    private List<Applicant> allApplicants = new ArrayList<>();
+    private List<Applicant> passedApplicants = new ArrayList<>();
+
+    public void process() {
+        for (int i = 0; i < allApplicants.size(); i++) {
+            Applicant applicant = allApplicants.get(i);
+            if (applicant.getScore() >= 80) {
+                passedApplicants.add(applicant);
+            }
+        }
+    }
+
+}
