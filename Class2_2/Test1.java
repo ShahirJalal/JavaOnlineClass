be68dd09af61c740866aafbcb5551b7f5995e404
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-25 01:13:57 +0800

    update

--- Class2_2/Test1.java
+++ Class2_2/Test1.java
@@ -1,5 +1,12 @@
 package Class2_2;
 
+import java.util.Date;
+
 public class Test1 {
-    
+    public static void main(String[] args) {
+        byte newAge = 30;
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(newAge);
+    }
 }
