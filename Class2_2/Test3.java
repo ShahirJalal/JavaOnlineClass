f856ba7256c94eac53bf524a01289c1a60e77f53
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-25 02:23:56 +0800

    Update

---
+++ Class2_2/Test3.java
@@ -0,0 +1,9 @@
+package Class2_2;
+
+public class Test3 {
+    public static void main(String[] args) {
+        String message = "Hello World" + "Mak kau hijau";
+        System.out.println(message.startsWith("K"));
+        System.out.println(message.length());
+    }
+}
