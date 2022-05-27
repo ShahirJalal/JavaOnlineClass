2c6a8e7f7972a21d8aec8dd0461c2eef12ac66f1
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-27 20:49:22 +0800

    Update Login.java

--- Class3/Class3/Login.java
+++ Class3/Class3/Login.java
@@ -1,13 +1,27 @@
 package Class3;
 
 import java.util.List;
+import java.text.BreakIterator;
 import java.util.ArrayList;
 
 public class Login {
     private List<User> allUser = new ArrayList<>();
-    if userNameInput == user1.newName{
+    if userNameInput == user1.newName {
         if userPasswordInput == user1.newPassword{
             System.out.println("You are logging in.");
+            Break;
+        }
+    }
+    else if {
+        if userPasswordInput == user1.newPassword{
+            System.out.println("You are logging in.");
+            Break;
+        }
+    }
+    else if {
+        if userPasswordInput == user1.newPassword{
+            System.out.println("You are logging in.");
+            Break;
         }
     }
 
